f9b5a8a484b918c1b64be52d1ee864640b60f5cb
janek-123 <jan.kocnar.022@pslib.cz>
2024-09-06 22:22:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Constantle</h1>
+      <p>It is like wordle, but only a bit worse and for mathematical constants</p>
+      
+    </>
+  )
+}
+
+export default App
